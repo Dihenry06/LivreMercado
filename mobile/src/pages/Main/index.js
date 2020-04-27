5fb2df98684ab2d9b361a2110023f0b1e9c64b3e
Otavio Souza <otavio.silva133@etec.sp.gov.br>
2020-04-27 16:53:50 -0300

    add routes to app mobile

---
+++ mobile/src/pages/Main/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+  View,
+  Text
+} from 'react-native';
+
+export default function Main() {
+  return (
+    <View>
+      <Text>
+        Meu pau
+      </Text>
+    </View>
+  )
+};
---
+++ mobile/src/routes.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// routes
+import LoginScreen from './auth/login';
+import RegisterScreen from './auth/register';
+
+const Stack = createStackNavigator();
+
+function Routes() {
+    return (
+        <Stack.Navigator screenOptions={{
+            headerShown: false
+        }}>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+        </Stack.Navigator>
+    );
+}
+
+export default Routes;
