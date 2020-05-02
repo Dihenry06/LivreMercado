0bfaf91a2df424bdb45eb865fcad55b68a2a1f0a
Diogo <diogo@MacBook-Pro-de-Diogo.local>
2020-05-02 18:34:08 -0300

    new session routes and update user

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,12 +2,36 @@ const routes = require('express').Router();
 const { celebrate, Segments, Joi } = require('celebrate');
 
 const UserController = require('./controllers/UserController');
+const SessionController = require('./controllers/SessionController');
 
+/**
+ * Route session
+ */
+routes.post('/session',celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        email: Joi.string().required().email(),
+        password: Joi.string().required().min(6),
+    })
+}),SessionController);
+
+/**
+ * Routes Users
+ */
 routes.post('/users', celebrate({
     [Segments.BODY]: Joi.object().keys({
         email: Joi.string().required().email(),
         password: Joi.string().required().min(6),
     })
 }), UserController.create);
+
+routes.put('/edit-user',celebrate({
+    [Segments.HEADERS]:Joi.object({
+        authorization : Joi.number().required()
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+        email:Joi.string().required().email(),
+        password: Joi.string().required().min(6),
+    })
+}),UserController.update);
 
 module.exports = routes;
