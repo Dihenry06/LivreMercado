b048e73a94b71116117e20ab13d03f9888267c37
Diogo <diogo@MacBook-Pro-de-Diogo.local>
2020-05-02 18:27:53 -0300

    user update

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,20 +1,37 @@
 const UserModel = require('../models/UserModel');
+const encryptsPassword = require('../utils/encryptsPassword');
 
 module.exports = {
 
     async create(req, res) {
-
         const { email, password } = req.body;
 
-        const data = { email, password };
+        const hash = await encryptsPassword.passwordCript(password);
+
+        const data = { email, hash };
 
         const response = await UserModel.create(data);
 
         if (!response) {
             return res.status(400).json({ error: 'error inserting' })
         }
-
         return res.status(200).json({ success: 'registered user' })
-    }
+    },
 
+    async update(req, res) {
+        const id = req.headers.authorization;
+        const { email, password } = req.body;
+
+        const hash = await encryptsPassword.passwordCript(password);
+
+        const data = { id, email, hash };
+
+        const response = await UserModel.update(data);
+
+        if (!response) {
+            return res.status(400).json({ error: 'error updating user' });
+        }
+
+        return res.status(200).json({ success: 'updating user '})
+    }
 }
--- backend/src/models/UserModel.js
+++ backend/src/models/UserModel.js
@@ -3,14 +3,27 @@ const connection = require('../database/connection');
 module.exports = {
 
     async create(data) {
-        const { email, password } = data;
+        const { email, hash } = data;
 
         await connection('users').insert({
             email,
-            password
+            password: hash
         });
 
         return true;
     },
+
+    async update(data) {
+        const { id, email, hash } = data;
+
+        const response = await connection('users')
+        .update({
+            email,
+            password: hash
+        })
+        .where('id', id);
+
+        return response;
+    }
 
 }
