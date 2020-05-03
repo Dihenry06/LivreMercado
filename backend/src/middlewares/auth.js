246fdeb452bd1429446c285aabcc2f2c03d01a86
dihenry06 <dihenry06@gmail.com>
2020-05-03 15:13:55 -0300

    fixed jwt

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -11,20 +11,22 @@ module.exports = (req, res, next) => {
     const parts = authToken.split(' ');
 
     if (!parts.length === 2) {
-        return res.status(401).json({ error: 'Token error' })
+        return res.status(401).json({ error: 'Token error' });
     }
 
-    const { scheme, token } = parts;
+    const [scheme, token] = parts;
 
-    if (!/^Baarer$^/i.test(scheme)) {
+    if (!/^Bearer$/i.test(scheme)) {
         return res.status(401).json({ error: 'Token malformatted.' });
     }
 
     jwt.verify(token, auth.secret, (err, decoded) => {
-        if(err) return res.status(401).json({ error : 'Token invalid' });
+        if (err) {
+            return res.status(401).json({ error: 'Token invalid' });
+        }
 
         req.userId = decoded.id;
         return next();
-    })
+    });
 
 }
