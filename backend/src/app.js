bb120c8361edf4afe6897ca7a7ae1e0892067310
Dihenry06 <dihenry06@gmail.com>
2020-05-24 18:17:21 -0300

    package morgan add

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 app.use('/api', require('./routes'));
